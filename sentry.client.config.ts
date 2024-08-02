1e08fabb5772994ae38d2aea9a59f0ce9efcbd80
Smais Shawon <smaisshawon@gmail.com>
2024-08-02 21:54:53 +0600

    modify next config to supress ts during build

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -5,14 +5,13 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e300b41df2ffb6d4592972400a25c0d3@o4506876178464768.ingest.us.sentry.io/4507159179034624",
+  dsn: "https://e768571162797dfdbe4a059af75e716c@o4507703176724480.ingest.us.sentry.io/4507703179476992",
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
-
   replaysOnErrorSampleRate: 1.0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
